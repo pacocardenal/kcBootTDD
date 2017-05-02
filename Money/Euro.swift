b60eab6d9dee96b681f5fa3ae735308f1995cf1b
Paco Cardenal <paco.cardenal@talentomobile.com>
2017-05-02 12:58:35 +0200

    Igualdad

--- Money/Euro.swift
+++ Money/Euro.swift
@@ -16,3 +16,25 @@ struct Euro {
         return Euro(amount: self.amount * numberTimes)
     }
 }
+
+extension Euro: Equatable {
+    public static func ==(lhs: Euro, rhs: Euro) -> Bool {
+        return lhs.amount == rhs.amount
+    }
+}
+
+extension Euro: Hashable {
+    public var hashValue: Int {
+        get {
+            return self.amount.hashValue
+        }
+    }
+}
+
+extension Euro: CustomStringConvertible {
+    public var description: String {
+        get {
+            return "\(self.amount) €"
+        }
+    }
+}
--- MoneyTests/MoneyTests.swift
+++ MoneyTests/MoneyTests.swift
@@ -4,11 +4,15 @@ import XCTest
 class MoneyTests: XCTestCase {
     
     var five: Euro!
+    var otherFive: Euro!
+    var ten: Euro!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         five = Euro(amount: 5)
+        otherFive = Euro(amount: 5)
+        ten = Euro(amount: 10)
     }
     
     override func tearDown() {
@@ -23,6 +27,12 @@ class MoneyTests: XCTestCase {
     func testSimpleMultiplication() {
         let ten = five.times(2)
         XCTAssertEqual(ten.amount, 10)
+    }
+    
+    func testEquality() {
+        XCTAssertEqual(otherFive, otherFive)
+        XCTAssertEqual(five, otherFive)
+        XCTAssertNotEqual(five, ten)
     }
     
 }
